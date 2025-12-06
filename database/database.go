74ac84978fe6726fcc35c2b1f8f287c6a2e6eb2d
xor27 <129921275+xor27@users.noreply.github.com>
2025-12-06 12:23:15 +0700

    fix db connection di railway

--- database/database.go
+++ database/database.go
@@ -3,34 +3,23 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "123"
-	dbname   = "bioskop_db"
-)
-
 func ConnectDB() {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host,
-		port,
-		user,
-		password,
-		dbname,
-	)
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		panic("DATABASE_URL tidak ditemukan! Pastikan sudah diset di Railway.")
+	}
 
 	var err error
-	DB, err = sql.Open("postgres", psqlInfo)
+	DB, err = sql.Open("postgres", databaseUrl)
 	if err != nil {
-		panic(err)
+		panic("Gagal membuka koneksi: " + err.Error())
 	}
 
 	err = DB.Ping()
